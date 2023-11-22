c06bee651c75f8dfaaed6ed621bda5d3162fb102
Adi <77723061+adishgarg@users.noreply.github.com>
2023-11-22 22:04:32 +0530

    bg image change

--- google_java.js
+++ google_java.js
@@ -1,6 +1,6 @@
 function myFunc() {
-    document.body.style.backgroundImage = "url(pexels-francesco-ungaro-1525041.jpg)";
+    document.body.style.backgroundImage = "url(22d48e78-a009-49ea-a7fb-3477ec06cd7e.jpeg)";
   }
   function myFunction() {
-    document.body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)),url(pexels-francesco-ungaro-1525041.jpg)";
+    document.body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)),url(22d48e78-a009-49ea-a7fb-3477ec06cd7e.jpeg)";
   }
