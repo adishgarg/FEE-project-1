f62b16bf0e4cb3a79639fdc8ba6d47366ef79545
Adi <77723061+adishgarg@users.noreply.github.com>
2023-12-05 13:37:00 +0530

    commit

--- google_java.js
+++ google_java.js
@@ -4,3 +4,15 @@ function myFunc() {
   function myFunction() {
     document.body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)),url(22d48e78-a009-49ea-a7fb-3477ec06cd7e.jpeg)";
   }
+
+  /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "350px";
+  document.getElementById("main").style.marginLeft = "350px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
